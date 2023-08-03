201cbb46f6a3a8595a852f5bbb00196289d86f99
William Ravelo M <drummerwilliam@gmail.com>
2023-08-02 21:33:42 -0500

    new network validator

--- entrega1/networks.rb
+++ entrega1/networks.rb
@@ -1,38 +1,30 @@
 require 'yaml'
 
 file = YAML.load_file('./docker-compose.yml')
-network_names = file['networks'].map{ |network| network[0] }
+
+networks_in_compose = []
 services = file['services']
+
 services_info = services.keys.map do |key|
-  {
-    name: key.strip,
-    networks: services[key]['networks'].compact
-  }
+  networks_in_compose += services[key]['networks'].compact.uniq
 end
 
-network_hash = {}
-services_info.each do |service|
-  service[:networks].each do |network_name|
-    network_hash[network_name] ||= []
-    network_hash[network_name] << service[:name]
-  end
-end
+app_network_correct = networks_in_compose.select{ |network| network.strip == 'app_net' }.count == 4
+user_network_correct = networks_in_compose.select{ |network| network.strip == 'user_net' }.count == 2
+route_network_correct = networks_in_compose.select{ |network| network.strip == 'route_net' }.count == 2
+post_network_correct = networks_in_compose.select{ |network| network.strip == 'post_net' }.count == 2
+offer_network_correct = networks_in_compose.select{ |network| network.strip == 'offer_net' }.count == 2
 
-only_five_networks = network_hash.values.count == 5
-global_network_exist = network_hash.values.select{ |services_in_network| services_in_network.uniq.count == 4  }.count == 1
-concrete_networks = network_hash.values.select{ |services_in_network| services_in_network.uniq.count == 2  }
-concrete_networks_exist = concrete_networks.count == 4
-
-all_service_in_only_one_concrete = services_info.all? do |service|
-  name = service[:name]
-  concrete_networks.select{ |network| network.include?(name) }.count == 1
-end
-
-
-all_conditions_met = global_network_exist && only_five_networks && concrete_networks_exist && all_service_in_only_one_concrete
-
-if !all_conditions_met
-  raise 'Bad networks'
+if !app_network_correct
+  raise 'App network not correct'
+elsif !user_network_correct
+  raise 'User network not correct'
+elsif !route_network_correct
+  raise 'Route network not correct'
+elsif !post_network_correct
+  raise 'Post network not correct'
+elsif !offer_network_correct
+  raise 'Offer network not correct'
 else
-  puts "OK"
+  puts 'OK'
 end
