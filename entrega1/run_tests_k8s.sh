719129baecce9b1e5b87b19ca8347fa1bcea64ba
César Forero <cesar.forero.gz@gmail.com>
2025-07-07 01:19:07 -0500

    feat: add script to run on k8s

---
+++ entrega1/run_tests_k8s.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+echo "------------------Enabling services------------------"
+
+ENV_VARS=()
+
+SERVICE = "users-app-service"
+
+if kubectl get svc "$SERVICE" >/dev/null 2>&1;
+  SELECTOR=$(kubectl get svc "$SERVICE" -o jsonpath='{.spec.selector.app}')
+  echo "Waiting for pods with selector app=$SELECTOR (from $SERVICE)..."
+  kubectl wait --for=condition=ready pod -l app="$SELECTOR" --timeout=30s
+  URL=$(minikube service "$SERVICE" --url)
+  ENV_VARS+=(--env-var "USERS_PATH=$URL")
+fi
+
+if [ ${#ENV_VARS[@]} -gt 0 ]; then
+  echo "🚀 Running Newman..."
+  newman run ".evaluator/entrega1.json" "${ENV_VARS[@]}" --verbose
+else
+  echo "❌ No valid services found for Newman run. Exiting."
+fi
