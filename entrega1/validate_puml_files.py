31f7a136fb1722c00b85a3cfdf1548178abdd32f
César Forero <cesar.forero.gz@gmail.com>
2025-07-12 20:33:15 -0500

    refactor(pulm): replace options lists by dict

--- entrega1/validate_puml_files.py
+++ entrega1/validate_puml_files.py
@@ -5,83 +5,83 @@ from pathlib import Path
 VALIDATIONS = [
     {
         "file": "docs/diagrams/components.puml",
-        "options": [
-            ["component users_app"],
-            ["component posts_app"],
-            ["component offers_app"],
-            ["component routes_app"],
-        ],
+        "options": {
+            "users_app":  ["component"],
+            "post_app":   ["component"],
+            "offers_app": ["component"],
+            "routes":     ["component"],
+        },
     },
     {
         "file": "docs/diagrams/entities.puml",
-        "options": [
-            ["entity users", "class user"],
-            ["entity posts", "class post"],
-            ["entity offers", "class offer"],
-            ["entity routes", "class route"],
-        ],
+        "options": {
+            "users":  ["entity", "class"],
+            "posts":  ["entity", "class"],
+            "offers": ["entity", "class"],
+            "routes": ["entity", "class"],
+        }
     },
     {
         "file": "docs/diagrams/deployment.puml",
-        "options": [
-            ["component users_app", "hexagon users_app"],
-            ["component posts_app", "hexagon posts_app"],
-            ["component offers_app", "hexagon offers_app"],
-            ["component routes_app", "hexagon routes_app"],
-            ["frame users-app", "component users-app"],
-            ["frame posts-app", "component posts-app"],
-            ["frame offers-app", "component offers-app"],
-            ["frame routes-app", "component routes-app"],
-            ["component users-app-service"],
-            ["component posts-app-service"],
-            ["component offers-app-service"],
-            ["component routes-app-service"],
-            ["database users_db"],
-            ["database posts_db"],
-            ["database offers_db"],
-            ["database routes_db"],
-            ["frame users-db", "component users-db"],
-            ["frame posts-db",  "component posts-db"],
-            ["frame offers-db", "component offers-db"],
-            ["frame routes-db", "component routes-db"],
-            ["component users-db-service"],
-            ["component posts-db-service"],
-            ["component offers-db-service"],
-            ["component routes-db-service"],
-            ["artifact users-db-volumen"],
-            ["artifact posts-db-volumen"],
-            ["artifact offers-db-volumen"],
-            ["artifact routes-db-volumen"],
-        ],
+        "options": {
+            "users_app":  ["component", "hexagon"],
+            "posts_app":  ["component", "hexagon"],
+            "offers_app": ["component", "hexagon"],
+            "routes_app": ["component", "hexagon"],
+            "users-app":  ["frame", "component"],
+            "posts-app":  ["frame", "component"],
+            "offers-app": ["frame", "component"],
+            "routes-app": ["frame", "component"],
+            "users_db":   ["database"],
+            "posts_db":   ["database"],
+            "offers_db":  ["database"],
+            "routes_db":  ["database"],
+            "users-db":   ["frame", "component"],
+            "posts-db":   ["frame", "component"],
+            "offers-db":  ["frame", "component"],
+            "routes-db":  ["frame", "component"],
+            "users-db-service":  ["component"],
+            "posts-db-service":  ["component"],
+            "offers-db-service": ["component"],
+            "routes-db-service": ["component"],
+            "users-app-service":  ["component"],
+            "posts-app-service":  ["component"],
+            "offers-app-service": ["component"],
+            "routes-app-service": ["component"],
+            "users-db-volumen":   ["artifact"],
+            "posts-db-volumen":   ["artifact"],
+            "offers-db-volumen":  ["artifact"],
+            "routes-db-volumen":  ["artifact"],
+        }
     },
     {
         "file": "docs/diagrams/networks.puml",
-        "options": [
-            ["component users_app", "frame users_app"],
-            ["component posts_app", "frame posts_app"],
-            ["component offers_app", "frame offers_app"],
-            ["component routes_app", "frame routes_app"],
-            ["frame users-app", "component users-app"],
-            ["frame posts-app", "component posts-app"],
-            ["frame offers-app", "component offers-app"],
-            ["frame routes-app", "component routes-app"],
-            ["component users-app-service"],
-            ["component posts-app-service"],
-            ["component offers-app-service"],
-            ["component routes-app-service"],
-            ["component users_db", "frame users_db"],
-            ["component posts_db", "frame posts_db"],
-            ["component offers_db", "frame offers_db"],
-            ["component routes_db", "frame routes_db"],
-            ["frame users-db", "component users-db"],
-            ["frame posts-db",  "component posts-db"],
-            ["frame offers-db", "component offers-db"],
-            ["frame routes-db", "component routes-db"],
-            ["component users-db-service"],
-            ["component posts-db-service"],
-            ["component offers-db-service"],
-            ["component routes-db-service"]
-        ],
+        "options": {
+            "users_app":  ["component", "frame"],
+            "posts_app":  ["component", "frame"],
+            "offers_app": ["component", "frame"],
+            "routes_app": ["component", "frame"],
+            "users-app":  ["frame", "component"],
+            "posts-app":  ["frame", "component"],
+            "offers-app": ["frame", "component"],
+            "routes-app": ["frame", "component"],
+            "users-app-service":  ["component"],
+            "posts-app-service":  ["component"],
+            "offers-app-service": ["component"],
+            "routes-app-service": ["component"],
+            "users_db":   ["frame", "component"],
+            "posts_db":   ["frame", "component"],
+            "offers_db":  ["frame", "component"],
+            "routes_db":  ["frame", "component"],
+            "users-db":   ["frame", "component"],
+            "posts-db":   ["frame", "component"],
+            "offers-db":  ["frame", "component"],
+            "routes-db":  ["frame", "component"],
+            "users-db-service":  ["component"],
+            "posts-db-service":  ["component"],
+            "offers-db-service": ["component"],
+            "routes-db-service": ["component"]
+        }
     },
 ]
 
@@ -94,17 +94,16 @@ def validate_puml(puml_file, options_list):
 
     content = path.read_text()
 
-    for options in options_list:
+    for key, value in options_list.items():
         found = False
-        print(f"🚀 Searching for: {options} in {puml_file}")
-        for option in options:
-            print(f"🚀 Searching for: '{option}'")
-            if option in content:
-                print(f"✅ Found: '{option}'")
+        print(f"🚀 Searching for: {key} in {puml_file}")
+        for option in value:
+            if f"{option} \"{key}\"" in content:
+                print(f"✅ Found: {option} \"{key}\"")
                 found = True
                 break
         if not found:
-            print(f"❌ None of the options found in {puml_file}: {options}")
+            print(f"❌ {key} not found in {puml_file}: {value}")
             return False
     return True
 
